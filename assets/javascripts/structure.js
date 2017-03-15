1c3feb39f94b3817039ebfba9f3772b0e15893ab
Daniel Röhers Moura <rohersmoura@gmail.com>
2017-03-15 01:07:59 -0300

    Update 2017-03-15T04:07:53.519Z

--- assets/javascripts/structure.js
+++ assets/javascripts/structure.js
@@ -1,1 +1,1 @@
-!function(t,e,n,i){"use strict";n(".vertical.menu").find("a").on("click",function(t){t.preventDefault();var e=this.getAttribute("href");"#"!==e&&(n(".tabs-panel").hide(),n(e).show())})}(window,document,jQuery);
+!function(t,e,n,i){"use strict";n(".vertical.menu").find("a").on("click",function(t){t.preventDefault();var e=this.getAttribute("href");"#"!==e&&(location.hash=e,n(".tabs-panel").hide(),n(e).show())});var a=location.hash;a&&n(a).show()}(window,document,jQuery);

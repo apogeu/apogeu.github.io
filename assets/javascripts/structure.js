b8c9bae06b0c28508ca1ae0937c2af7c7d3936a0
Daniel Röhers Moura <rohersmoura@gmail.com>
2017-03-15 01:07:26 -0300

    app structure

--- assets/javascripts/structure.js
+++ assets/javascripts/structure.js
@@ -6,9 +6,15 @@
     e.preventDefault();
     var href = this.getAttribute('href');
     if (href !== '#') {
+      location.hash = href;
       $('.tabs-panel').hide();
       $(href).show();
     }
   });
+
+  var hash = location.hash;
+  if (hash) {
+    $(hash).show();
+  }
 
 })(window, document, jQuery);
